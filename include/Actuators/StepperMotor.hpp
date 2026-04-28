52d22cb8c203e76e07b094e1dd990fb6c69a3406
HermanGran <142888421+HermanGran@users.noreply.github.com>
2026-04-28 19:48:26 +0200

    Added comments to exiplain classes and functions

--- include/Actuators/StepperMotor.hpp
+++ include/Actuators/StepperMotor.hpp
@@ -9,26 +9,119 @@
 #include <AccelStepper.h>
 #include <Sensors/CurrentSensor.hpp>
 
+
+/**
+ * @brief Moves the stepper motor to the specified target position.
+ *
+ * This function instructs the motor to move to the given position by calculating
+ * the required steps. It utilizes acceleration and deceleration to ensure smooth
+ * motion. The motor runs until the target position is reached.
+ *
+ * @param position Target position to move the motor to, in steps.
+ */
 class StepperMotor {
 public:
+
+    /**
+     * @brief Constructor for the stepper motor class.
+     *
+     * This class has to take in the below parameters to function properly, the current sensor
+     * is used to detect of the motor is colliding with something.
+     *
+     * @param EN_PIN Enable pin for the motor driver
+     * @param DIR_PIN Direction pin for the motor driver
+     * @param STEP_PIN Step pin for the motor driver
+     * @param currentSensor Current sensor object
+     */
     StepperMotor(int EN_PIN, int DIR_PIN, int STEP_PIN, CurrentSensor& currentSensor);
 
+    /**
+     * @brief Initializes the stepper motor and its settings.
+     *
+     * Configures the stepper motor hardware by initializing its control pins,
+     * setting the default speed, acceleration, and pin configuration. It also disables the motor outputs
+     * to ensure safety until the motor is explicitly enabled. This avoids unnecessary power usage and prevents
+     * the motor driver board from overheating.
+     *
+     * @note This method must be invoked before any motion-related functions to properly initialize the
+     * stepper motor hardware.
+     */
     void init();
 
+    /**
+     * @brief Function for manually setting the motor soeed
+     *
+     * This function is used to setting the speed manually, mainly used inside the homing function
+     * in the gripper class.
+     *
+     * @param speed Speed for the motor to drive with
+     */
     void setSpeed(int speed);
 
+    /**
+     * @brief Function for setting acceleration of the stepper motor
+     *
+     * This function sets the acceleration of the stepper motor. This is mainly used when using the runToPosition
+     * function within the AccelStepper library, which accelerates and decelerates when running.
+     *
+     * @param acceleration Acceleration of the stepper motor
+     */
     void setAcceleration(int acceleration);
 
+    /**
+     * @brief Function for setting a home position
+     *
+     * This function is used for homing the stepper motor, places the current position of the motor
+     * as it's new home (0).
+     */
     void setHomePos();
 
+    /**
+     * @brief Executes the stepping motion of the stepper motor if it is currently running.
+     *
+     * This method checks if the stepper motor is in motion. If the motor is running,
+     * it calls the `run()` function of the `AccelStepper` instance to advance the motor
+     * position based on the current step instructions. It continuously monitors the
+     * remaining distance to the target. Once the motor reaches its target position and no
+     * further steps are required, it stops the motor by disabling its outputs and updates
+     * the internal `running_` flag to `false`.
+     *
+     * @note This method should be called periodically in a loop or task to ensure
+     * smooth movement and precise control of the stepper motor.
+     */
     void run();
 
+    /**
+     * @brief Stops the motion of the stepper motor.
+     *
+     * This method halts the current movement of the stepper motor immediately by invoking the
+     * `stop()` function of the underlying `AccelStepper` instance. It does not reset the target
+     * position but ensures the motor stops moving at its current step. This can be useful in
+     * emergency cases or when an immediate stop is required.
+     *
+     * @note Ensure that stopping the motor does not cause abrupt operational issues such as
+     * mechanical stress or position misalignments in the motor's system.
+     */
     void stop();
 
-    void runToPosition(const int position);
+    /**
+     *
+     * @param position
+     */
+    void runToPosition(int position);
 
+
+    /**
+     *
+     * @return
+     */
     AccelStepper* getStepper();
 
+    /**
+     * @brief
+     *
+     * @return Boolean status, running or stopped
+     */
     bool isRunning() const;
 
 private:
--- include/Gripper/Gripper.hpp
+++ include/Gripper/Gripper.hpp
@@ -14,20 +14,88 @@
 #define GRIPPER_LATCH 1
 #define GRIPPER_RELEASE 2
 
+/**
+ * @class Gripper
+ *
+ * @brief A class to control a robotic gripper controlled by a stepper motor,
+ *        limit switch, and current sensor. Provides functionality for initialization,
+ *        homing, latching, releasing, and state management.
+ */
 class Gripper {
 public:
-    Gripper();
-    void init();
-    void homing();
-    void latch();
-    void release();
-    bool isClosed(); // To be added later, must include the current sensor to sense latching
 
+    Gripper();
+
+    /**
+     * @brief Initializes the object
+     *
+     * This function prepares the object for use, initializing the necessary parameters and object
+     * used by this class, such as stepper motor, limit-switch, and current sensor.
+     */
+    void init();
+
+    /**
+     * @brief Initiates the homing sequence to calibrate the gripper's starting position.
+     *
+     * This method moves the gripper to its home position by driving the stepper motor
+     * until the limit switch is triggered, then adjusts for precise positioning. The
+     * stepper motor's outputs are enabled at the start and disabled upon completion.
+     *
+     * The homing process involves:
+     * - Moving the stepper motor in reverse at a specific speed until the limit switch is pressed.
+     * - Logging critical stages of the homing procedure for troubleshooting and verification.
+     * - Once the limit switch has been pressed, backing off until the switch is released.
+     * - Setting the home position using the StepperMotor's internal calibration method.
+     *
+     * @warning Manual intervention during the homing process may cause incorrect calibration
+     */
+    void homing();
+
+    /**
+     * @brief Moves the gripper to the latching position
+     *
+     * This method latches the gripper by running the stepper motor to a pre-defined position.
+     *
+     * @note Ensure the gripper is initialized and homed before calling this method
+     *       to avoid inaccuracies in positioning.
+     *
+     * @warning Improper initialization or usage may result in incorrect positioning or
+     *          potential damage to the gripper mechanism. If the stepper motor slips,
+     *          run home to avoid damaging the gripper when releasing.
+     */
+    void latch();
+
+    /**
+     * @brief Releases the gripper by moving it to the open position (home pos)
+     *
+     * This method moves the gripper to the home position and releasing the gripper.
+     *
+     * @note This method assumes the gripper has been initialized and is properly
+     * calibrated to accurately respond to positional commands.
+     */
+    void release();
+
+    /**
+     * @brief Getter function for the stepper motor object within this class.
+     *
+     * @return A reference of the StepperMotor object.
+     */
     StepperMotor& getStepper();
 
+    /**
+     * @brief Getter function for the current sensor object within this class.
+     *
+     * @return A reference of the CurrentSensor object.
+     */
     CurrentSensor& getCurrentSensor();
 
+    /**
+     * @brief Getter function for the limit switch object within this class
+     *
+     * @return A reference of the LimitSwitch object
+     */
     LimitSwitch& getLimitSwitch();
+
     TaskHandle_t callerTaskHandle_ = nullptr;
     volatile bool tasksRunning_ = false;
 private:
